c8cbee740e0fcc9082bd8a7aa0aa9d4b74a40f18
TJ Horner <rowenrocks@gmail.com>
2014-01-09 20:47:57 -0800

    that didnt work

--- magic.js
+++ magic.js
@@ -1,9 +1,11 @@
+var numSparks;
 var magic = {
 	use: function(sparks){
 		count = 0;
+		numSparks = sparks;
 		var counter = setTimeout(function(){
 			count++;
-			if(count != sparks){
+			if(count != numSparks){
 				console.log('(sparks and crap)');
 			}else{
 				clearTimeout(counter);
