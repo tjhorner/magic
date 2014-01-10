037ff31a28e376d8f7c5afd95418469bf7ed244a
TJ Horner <rowenrocks@gmail.com>
2014-01-09 20:46:13 -0800

    Update magic.js

--- magic.js
+++ magic.js
@@ -1,5 +1,14 @@
 var magic = {
-	use: function(){
+	use: function(sparks){
+		count = 0;
+		var counter = setTimeout(function(){
+			count++;
+			if(count != sparks){
+				console.log('(sparks and crap)');
+			}else{
+				clearTimeout(counter);
+			}
+		});
 		return '(sparks and crap)';
 	}
 }
