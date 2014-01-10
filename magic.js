db9be9028f1499e69604e4275a002c4cdaffbd56
TJ Horner <rowenrocks@gmail.com>
2014-01-09 20:50:54 -0800

    Update magic.js

--- magic.js
+++ magic.js
@@ -1,7 +1,7 @@
 var magic = {
 	use: function(sparks){
 		for(var i=0; i < sparks; i++){
-			console.log('#' + i + '. (sparks and crap)');
+			console.log('#' + i+1 + '. (sparks and crap)');
 		}
 		return '(sparks and crap)';
 	}
